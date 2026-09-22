9ebc0ef94023131b80d61fe3010cad2f5571d286
Fadhil Majid Mu'tashim <fadhil.majid.2405356@students.um.ac.id>
2026-09-22 22:20:31 +0700

    fix(database): tambah HasUuids dan field role di model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasUuids, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
